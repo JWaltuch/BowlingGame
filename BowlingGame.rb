2468ef8ffa24081057fbb3f0f4418ef8f0c23e81
JWaltuch <jenna.waltuch@gmail.com>
2019-03-04 10:36:19 -0500

    Added some player greeting and interaction ideas

--- BowlingGame.rb
+++ BowlingGame.rb
@@ -1,4 +1,4 @@
-Class Game
+class Game
   def roll(pins_knocked_down)
     pins_remaining = 10 - pins_knocked_down
     pins_knocked_down = rand(1..pins_remaining)
@@ -9,7 +9,7 @@ Class Game
   end
 end
 
-Class Frame
+class Frame
   def score(first_pins, second_pins, strike, spare)
     score += score + first_pins + second_pins
     if strike
@@ -35,7 +35,7 @@ end
 #   end
 # end
 
-Class TenthFrame
+class TenthFrame
   def score(first_pins, second_pins, third_pins)
     score += score + first_pins + second_pins + third_pins
   end
@@ -47,6 +47,7 @@ def main
   greet_player
   i = 0
   while i <= 10
+    ask_player_to_roll
     pins_knocked_down = 0
     pins_knocked_down = Game.roll(pins_knocked_down)
     add_to_frame_score(pins_knocked_down)
@@ -55,6 +56,17 @@ def main
     display_frame_score
     i += 1
   end
-
-
 end
+
+def greet_player
+  puts("Welcome, player. Let's bowl!")
+end
+
+def ask_player_to_roll
+  puts("Type 'Roll' to roll the ball. Type 'Quit' to quit.")
+  roll = gets.chomp.to_s
+  if roll == "Quit"
+    puts("How can I let the player quit?")
+end
+
+main
